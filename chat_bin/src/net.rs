45adf44792d5a21dc9e9574030b89257573357f6
Tilman <thoh@gmx.net>
2025-09-25 22:08:26 +0200

    send and receive mqtt messages works

--- chat_bin/src/net.rs
+++ chat_bin/src/net.rs
@@ -2,8 +2,8 @@ use std::{io, net::SocketAddr, time::Duration};
 
 use config::Config;
 use log::{debug, info};
-use rumqttc::{v5::Incoming, AsyncClient, MqttOptions, NetworkOptions, QoS};
-use tokio::net::{self};
+use rumqttc::{EventLoop, Incoming, AsyncClient, MqttOptions, NetworkOptions, QoS};
+use tokio::{net::{self}, task, time};
 use chat_lib::net::Message;
 
 pub async fn mqtt_connect(config: Config) -> ! {
@@ -36,36 +36,65 @@ pub async fn mqtt_connect(config: Config) -> ! {
 
         client.subscribe("chat/#", QoS::ExactlyOnce).await.unwrap();
 
+        task::spawn(async move {
+            mqtt_eventloop(eventloop).await;
+        });
+
+        task::spawn(async move {
+            mqtt_client(client).await;
+        });
+        
         loop {
-            let notification = match eventloop.poll().await {
-                Ok(x) => x,
-                Err(e) => {
-                    println!("{:?}", e);
-                    break
-                },
-            };
-            println!("Received = {:?}", notification);
-
-            let packet = match notification {
-                rumqttc::Event::Outgoing(outgoing) => continue,
-                rumqttc::Event::Incoming(packet) => packet,
-            };
-
-            let publish = match packet {
-                rumqttc::Packet::Publish(publish) => publish,
-                _ => continue,
-            };
-
-            let message: Message = match bincode::decode_from_slice(&publish.payload, bincode::config::standard()) {
-                Ok(x) => x.0,
-                Err(e) => {
-                    println!("{}", e);
-                    continue
-                }
-            };
-
+            tokio::time::sleep(Duration::from_secs(5)).await;
         }
     }
-    
+}
 
+async fn mqtt_eventloop(mut eventloop: EventLoop) {
+    loop {
+        let notification = match eventloop.poll().await {
+            Ok(x) => x,
+            Err(e) => {
+                println!("{:?}", e);
+                break
+            },
+        };
+        println!("Received = {:?}", notification);
+
+        let packet = match notification {
+            rumqttc::Event::Outgoing(outgoing) => {
+                println!("outgoing: {:?}", outgoing);
+                continue;
+            },
+            rumqttc::Event::Incoming(packet) => packet,
+        };
+
+        let publish = match packet {
+            rumqttc::Packet::Publish(publish) => publish,
+            _ => continue,
+        };
+
+        println!("topic:{}", &publish.topic);
+
+        let message: Message = match bincode::decode_from_slice(&publish.payload, bincode::config::standard()) {
+            Ok(x) => x.0,
+            Err(e) => {
+                println!("{}", e);
+                continue
+            }
+        };
+
+        println!("message: {}", message);
+
+    }
+}
+
+async fn mqtt_client(client: AsyncClient) {
+    loop {
+        println!("test client");
+        let message = Message::new("test".to_string(), "tt".to_string());
+        let encoded_message = bincode::encode_to_vec(message, bincode::config::standard()).unwrap();
+        client.publish("chat/1", rumqttc::QoS::AtMostOnce, false, encoded_message).await.unwrap();
+        tokio::time::sleep(Duration::from_secs(5)).await;
+    }
 }
--- chat_lib/src/net.rs
+++ chat_lib/src/net.rs
@@ -1,3 +1,5 @@
+use std::ffi::os_str::Display;
+
 use bincode::{Decode, Encode};
 use serde::{Deserialize, Serialize};
 
@@ -5,4 +7,20 @@ use serde::{Deserialize, Serialize};
 pub struct Message {
     sender: String,
     text: String,
+}
+
+impl Message {
+    pub fn new(sender: String, text: String) -> Self {
+        Message {
+            sender,
+            text,
+        }
+    }
+}
+
+// Implementing the Display trait for Message
+impl core::fmt::Display for Message {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{}: {}", self.sender, self.text)
+    }
 }
